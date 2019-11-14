a9a3753aa6d5dc8eeda011284067bb049a1c9779
Bahrudin Hrnjica <bhrnjica@bahr-sb2.com>
2019-11-14 15:30:51 +0100

    Add Extension method GetLabels from the output schema-

--- src/daany.ext/DataFrameExt.cs
+++ src/daany.ext/DataFrameExt.cs
@@ -84,6 +84,30 @@ namespace Daany.Ext
             return newDf;
         }
 
+        public static IEnumerable<string> GetLabels(DataViewSchema schema)
+        {
+            VBuffer<ReadOnlyMemory<char>> labels = new VBuffer<ReadOnlyMemory<char>>();
+
+            foreach (var d in schema)
+            {
+                if (d.HasKeyValues())
+                {
+                    d.GetKeyValues(ref labels);
+                    var predLables = labels.DenseValues().Select(x => x.ToString());
+                    return predLables;
+                }
+                else if (d.HasSlotNames())
+                {
+                    d.GetSlotNames(ref labels);
+                    var predLables = labels.DenseValues().Select(x => x.ToString());
+                    return predLables;
+                }
+            }
+
+            //this should not heppend
+            throw new Exception("No category labels are defined.");
+        }
+
     }
 
    
