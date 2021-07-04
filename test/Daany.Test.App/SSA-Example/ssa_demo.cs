89d08f067095802a0a98ab35b69c8c49e4368db3
bhrnjica <bhrnjica@BAHR-NB01>
2021-07-04 20:25:23 +0200

    nuget packing and publish implementation

---
+++ test/Daany.Test.App/SSA-Example/ssa_demo.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Globalization;
+using Microsoft.ML.Data;
+using Microsoft.ML;
+using Microsoft.ML.Transforms;
+
+using Accord.Math;
+using System.Threading.Tasks;
+using OxyPlot;
+using System.Windows.Forms;
+using Daany;
+using Daany.MathStuff;
+using Daany.Stat;
+using Daany.Plot;
+
+namespace ML.Net.App.TimeSeries
+{
+    //Singular Sectrum Analysis
+    public class SSADemo
+    {
+        static string root = "..\\..\\..\\..\\..\\dataset"; 
+
+        public static void Forecasting()
+        {
+            //
+            var strPath = $"AirPassengers.csv";
+            var mlDF = Daany.DataFrame.FromCsv(strPath, sep: ',');
+            var ts = mlDF["#Passengers"].Select(f => Convert.ToDouble(f));//create time series
+
+            //create Singular Spectrum Analysis object
+            var ssa = new SSA(ts);
+            //perform analysis
+            ssa.Fit(36);
+
+            //helper vars to hold data
+            int n = 4;
+            var setIndex = Enumerable.Range(0, n);
+            double[][] ts_components = new double[setIndex.Count()][];
+
+            //cumulative components array
+            double[] cumTS = new double[ts.Count()];
+
+            //take the first n components and plot it
+            var models = new List<PlotModel>();
+            foreach (var i in setIndex)
+            {
+                ts_components[i] = ssa.Reconstruct(i+1);
+                cumTS = cumTS.Add(ts_components[i]);
+                var m11 = Daany.Plot.Chart.LinePlot("", $"TS Components {i+1}",
+                    Enumerable.Range(1,ts_components[i].Length).Select(t=>(double)t).ToArray(), ts_components[i],System.Drawing.Color.Blue, OxyPlot.MarkerType.Plus);
+                //
+                models.Add(m11);
+            }
+            //plot component together
+
+            ChartComponent.ShowPlots("Passenger TS Components",models.ToArray()).Wait();
+            
+            //plot sum of the components
+            var x = Enumerable.Range(1, cumTS.Length).Select(t => (double)t).ToArray();
+            var m1 = Daany.Plot.Chart.LineSeries("Predicted SSA",
+                    x,cumTS, System.Drawing.Color.Blue, OxyPlot.MarkerType.Plus);
+            var m2 = Daany.Plot.Chart.LineSeries("Original Time Series",
+                   x, ts.ToArray(), System.Drawing.Color.Red, OxyPlot.MarkerType.Plus);
+
+            var model = new PlotModel();
+            model.Series.Add(m1);
+            model.Series.Add(m2);
+            ChartComponent.ShowPlots("Passenger TS vs Predicted",model).Wait();
+
+            //Forecasting of time series
+            var stepAHead = 12;
+            var predictedTS = ssa.Reconstruct(15);
+            var predicted = ssa.Forecast(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 }, stepAHead);
+
+            //create three plotmodel: actual TS, predicted TS, Forcast ts ahead
+            var mm3 = Daany.Plot.Chart.LineSeries("Predicted Time Series",
+                  x, predictedTS, System.Drawing.Color.Green, OxyPlot.MarkerType.Plus);
+            var mm2 = Daany.Plot.Chart.LineSeries("Original Time Series",
+                   x, ts.ToArray(), System.Drawing.Color.Red, OxyPlot.MarkerType.Plus);
+            
+            //forecast steps ahead
+            var x1 = Enumerable.Range(cumTS.Length, stepAHead+1).Select(t => (double)t).ToArray();
+            //start with the last point form the times series
+            var y = predicted.Skip(ts.Count()-1).Take(stepAHead+1).ToArray();
+            var mm1 = Daany.Plot.Chart.LineSeries($"Forecast of {stepAHead}",
+                   x1, y, System.Drawing.Color.Blue, OxyPlot.MarkerType.Plus);
+            //add series to model 
+            var model1 = new PlotModel();
+            model1.Series.Add(mm1);
+            model1.Series.Add(mm2);
+            model1.Series.Add(mm3);
+
+            //plot forecasting
+            ChartComponent.ShowPlots("Passenger TS vs Forecasting", model1).Wait();
+        }
+    }
+
+  
+}
---
+++ test/Daany.Test.App/chart/ExampleChart.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+namespace Daany.Plot
+{
+    public class ExampleOXYPlot
+    {
+        public static void Run()
+        {
+            var outputToFile = "test-oxyplot-static-export-file";
+            var outputExportStreamOOP = "test-oxyplot-export-stream";
+
+            var width = 1024;
+            var height = 768;
+            var resolutions = new[] { 72d, 96d, 182d };
+
+            var model = HeatMapExample();//BuildPlotModel();
+
+            foreach (var resolution in resolutions)
+            {
+                // export using the instance methods
+                using (var stream = new MemoryStream())
+                {
+                    var strPath = $"{outputExportStreamOOP}{resolution}.svg";
+                    var strFull = Path.Combine(Directory.GetCurrentDirectory(), strPath);
+                    var jpegExporter = new SvgExporter();
+                    jpegExporter.Export(model, stream);
+                    System.IO.File.WriteAllBytes(strFull, stream.ToArray());
+                    Process.Start(@"cmd.exe ", @"/c " + strFull);
+                }
+            }
+        }
+
+        private static PlotModel BuildPlotModel()
+        {
+            var rand = new Random(21);
+
+            var model = new PlotModel { Title = "Cake Type Popularity" };
+
+            var cakePopularity = Enumerable.Range(1, 5).Select(i => rand.NextDouble()).ToArray();
+            var sum = cakePopularity.Sum();
+            var barItems = cakePopularity.Select(cp => RandomBarItem(cp, sum)).ToArray();
+            var barSeries = new BarSeries
+            {
+                ItemsSource = barItems,
+                LabelPlacement = LabelPlacement.Base,
+                LabelFormatString = "{0:.00}%"
+            };
+
+            model.Series.Add(barSeries);
+
+            model.Axes.Add(new CategoryAxis
+            {
+                Position = AxisPosition.Left,
+                Key = "CakeAxis",
+                ItemsSource = new[]
+                {
+                "Apple cake",
+                "Baumkuchen",
+                "Bundt Cake",
+                "Chocolate cake",
+                "Carrot cake"
+            }
+            });
+
+            return model;
+        }
+
+        private static BarItem RandomBarItem(double cp, double sum)
+            => new BarItem { Value = cp / sum * 100, Color = RandomColor() };
+
+        private static OxyColor RandomColor()
+        {
+            var r = new Random();
+            return OxyColor.FromRgb((byte)r.Next(255), (byte)r.Next(255), (byte)r.Next(255));
+        }
+    
+       
+        
+        public static PlotModel HeatMapExample()
+        {
+            var model = new PlotModel { Title = "Cakes per Weekday" };
+
+            // Weekday axis (horizontal)
+            model.Axes.Add(new CategoryAxis
+            {
+                Position = AxisPosition.Bottom,
+
+                // Key used for specifying this axis in the HeatMapSeries
+                Key = "WeekdayAxis",
+
+                // Array of Categories (see above), mapped to one of the coordinates of the 2D-data array
+                ItemsSource = new[]
+                {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+    }
+            });
+
+            // Cake type axis (vertical)
+            model.Axes.Add(new CategoryAxis
+            {
+                Position = AxisPosition.Left,
+                Key = "CakeAxis",
+                ItemsSource = new[]
+                {
+            "Apple cake",
+            "Baumkuchen",
+            "Bundt cake",
+            "Chocolate cake",
+            "Carrot cake"
+    }
+            });
+
+            // Color axis
+            model.Axes.Add(new LinearColorAxis
+            {
+                Palette = OxyPalettes.Hot(200)
+            });
+
+            var rand = new Random();
+            var data = new double[7, 5];
+            for (int x = 0; x < 5; ++x)
+            {
+                for (int y = 0; y < 7; ++y)
+                {
+                    data[y, x] = rand.Next(0, 200) * (0.13 * (y + 1));
+                }
+            }
+
+            var heatMapSeries = new HeatMapSeries
+            {
+                X0 = 0,
+                X1 = 6,
+                Y0 = 0,
+                Y1 = 4,
+                XAxisKey = "WeekdayAxis",
+                YAxisKey = "CakeAxis",
+                RenderMethod = HeatMapRenderMethod.Rectangles,
+                LabelFontSize = 0.2, // neccessary to display the label
+                Data = data
+            };
+
+            model.Series.Add(heatMapSeries);
+
+            return model;
+        }
+    
+    }
+
+}
